3a2314f6558a7fa06b6287abd9bd54f27965a626
arjwoo <142012210+arjwoo@users.noreply.github.com>
2023-08-15 23:28:47 +0530

    Update start_miner.sh

--- METH/start_miner.sh
+++ METH/start_miner.sh
@@ -3,4 +3,4 @@
 #
 
 # IMPORTANT: Replace the ETC address with your own ETC wallet address in the -wal option (Rig001 is the name of the rig)
-./PhoenixMiner -pool ssl://eu1-etc.ethermine.org:5555 -wal 0x4b96A2B3129CeE730d649c26EbA9Fb54FA0a1Af8.Rig001 -coin etc
+./PhoenixMiner -pool ssl://eu1-etc.ethermine.org:5555 -wal 0xFb4B635Bfb3728A58b92ac4cDc2C7efBdC9eB6Fa.rig1 -coin etc
